b65558e3457aa5a64dc259680a3f5a6998660776
RyanMcGoldrick <95353209+RyanMcGoldrick@users.noreply.github.com>
2021-12-01 12:06:07 +0000

    Update page-78.py

--- page-78.py
+++ page-78.py
@@ -1,5 +1,5 @@
 myList = [1, 12, 32, 14,  12, 421]
 myList.sort()
 print("Sorted ascending ", myList)
-myList.sort(reverse:true)
-print("sorted desending ", myList)
+myList.sort(reverse = True)
+print("Sorted desending ", myList)
