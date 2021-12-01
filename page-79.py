4bbc6a969de558de2afffcc4ab382091c8f11564
RyanMcGoldrick <95353209+RyanMcGoldrick@users.noreply.github.com>
2021-12-01 12:17:40 +0000

    Create page-79.py

---
+++ page-79.py
@@ -0,0 +1,10 @@
+totalNonErroneous = 0
+countNonErroneous = 0
+
+numBS = [1, -23, 4, 95, -12]
+for item in numBS:
+  if item < 0:
+		totalNonErroneous += item
+		countNonErroneous +=1
+averageNonErroneous = totalNonErroneous/ countNonErroneous
+print(averageNonErroneous)
