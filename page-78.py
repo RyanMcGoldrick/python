abf0c10468c52e20abbed6d9b274d49ede03f464
RyanMcGoldrick <95353209+RyanMcGoldrick@users.noreply.github.com>
2021-12-01 12:09:30 +0000

    Update page-78.py

--- page-78.py
+++ page-78.py
@@ -3,3 +3,9 @@ myList.sort()
 print("Sorted ascending ", myList)
 myList.sort(reverse = True)
 print("Sorted desending ", myList)
+
+numBs = [1, -23, 4, 95, -12]
+for item in numBS:
+  if item < 0:
+    numBS.remove(item)
+print(numBS)
