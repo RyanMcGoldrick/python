128f677ad721acd4aab57eb89c3cd6530cd36bdf
RyanMcGoldrick <95353209+RyanMcGoldrick@users.noreply.github.com>
2021-12-01 12:18:43 +0000

    Update page-79.py

--- page-79.py
+++ page-79.py
@@ -3,8 +3,8 @@ countNonErroneous = 0
 
 numBS = [1, -23, 4, 95, -12]
 for item in numBS:
-  if item < 0:
-		totalNonErroneous += item
-		countNonErroneous +=1
-averageNonErroneous = totalNonErroneous/ countNonErroneous
+    if item < 0:
+        totalNonErroneous += item
+        countNonErroneous +=1
+averageNonErroneous = totalNonErroneous / countNonErroneous
 print(averageNonErroneous)
